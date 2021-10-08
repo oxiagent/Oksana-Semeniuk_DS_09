5a5fcb551db1ec447c3e1f6303e09bd1968805bd
oxiagent <bestregards.oksana@gmail.com>
2021-10-08 16:21:42 +0300

    08.10.2021

---
+++ Lab_1/Homework 1 — Oksana Semeniuk.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1. Markdown cells
+# * Hi! It's Main Academy?
+# * All HTML tabs are supported 
+# * Моя сторінка на лінкедін [Лінкедін](https://www.linkedin.com/in/semeniuk-oksana-9a29a2b5/)   
+# # This is Heading1  
+# ## This is Heading2
+# 
+#    some gray colored text
+# > This where we can bring quotes and present them nicely
+# 
+# And this is the paragraph about something very important. Even math: $$ y_{sub}=\sqrt[]{(x_{sub})} $$
+# 
+#                                                                                                                       
+#           
+# Or python code example:
+# 
+#     import math
+#     def foo():
+#         return "foo"
+
+# Here is the sample mean:  
+#  $$ \overline{y} = \frac{1}{n}\, \sum_{i=1}^{n}\, y_{i} $$  
+#  Here is the sample variance: 
+#  $$ \sigma^2 = \frac{\sum_{i=1}^{n} (y_{i} - \bar{y})^2}{n-1} $$  
+#  Here is the sample standart deviation: 
+#  $$ \sigma = \sqrt[]{\frac{\sum_{i=1}^{n} (y_{i} - \bar{y})^2}{n-1}} $$  
+
+# In[ ]:
+
+
+
+
